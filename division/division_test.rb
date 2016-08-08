8aad2a56b9c7fedb10d3f0b05061e54ecc88d477
roofner <steelerbaby210@hotmail.com>
2016-08-08 13:52:41 -0400

    test div

--- division/division_test.rb
+++ division/division_test.rb
@@ -8,4 +8,10 @@ class TestDivision< Minitest::Test
 	def test_div_negative_7_equals_14_div_neg_2
 		assert_equal(-7, my_division(-14,2))
 	end
+	def test_div_zero_div_anynum_100
+		assert_equal(0,my_division(0,100))
+	end
+	def test_2_divde_0_equal_infinity
+		assert_equal("Can not divide by zero",my_division(2,0))
+	end
 end
